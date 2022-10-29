753d5bbe341a8b4f66914635ed0c773f24fe7159
Charles Marques <charles_sjp@yahoo.com.br>
2022-10-29 14:49:20 -0300

    first and only commit

---
+++ src/Components/Container/index.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import styled from "styled-components";
+import Titulo from "../Titulo";
+import Conta from "../Conta";
+import Extrato from "../Extrato";
+
+const ContainerDiv = styled.div`
+  background-color: ${ ( { theme } ) => theme.body };
+  min-height: 90vh;
+  padding: 0px 15vw;
+`;
+const Conteudo = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  color:  ${ ( { theme } ) => theme.text };
+
+  @media (max-width: 800px) {
+    flex-direction: column;
+  }
+`;
+const Container = () => {
+  return (
+    <ContainerDiv>
+      <Titulo>Olá Fulano!</Titulo>
+      <Conteudo>
+        <Conta />
+        <Extrato/>
+      </Conteudo>
+    </ContainerDiv>
+  );
+};
+
+export default Container;
